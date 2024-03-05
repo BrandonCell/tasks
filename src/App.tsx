d01fbb2d01f4fa4d37f99aaf1509524d2caed2e5
Brandon Cell <bcell@udel.edu>
2024-03-05 18:24:55 -0500

    first component fixed

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App(): JSX.Element {
             </header>
 
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
@@ -38,7 +38,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
-
         </div>
     );
 }
